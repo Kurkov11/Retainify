96913f02d434a7d02a3c1203ac2ca65e76d5e3e7
Damian Kurkowicz <kurkovsw@icloud.com>
2025-10-04 15:06:33 +0200

    add basic pagination

--- retainify/src/app/reader/page.tsx
+++ retainify/src/app/reader/page.tsx
@@ -1,14 +1,65 @@
 "use client";
 
 import { FileInput } from "@/components/FileInput";
+import { createLoader, Reader, Book } from "@asteasolutions/epub-reader";
+
 import "./reader.css";
+import { useState } from "react";
 
 export default function Page() {
+  const [reader, setReader] = useState<Reader | null>(null);
   return (
     <>
       <h1>Book reader</h1>
-      <FileInput />
-      <div id="epub-reader" className="w-[800px] h-[800px]"></div>
+      <FileInput
+        onChange={(e) => {
+          const file = e.target?.files?.[0];
+          console.log(file);
+          if (!file) {
+            return;
+          }
+
+          (async () => {
+            try {
+              const container = document.getElementById("epub-reader");
+              if (!container) return <></>;
+
+              const loader = await createLoader(file);
+              Book.open(loader).subscribe({
+                next: (book) => {
+                  const newReader = new Reader(container, book);
+                  setReader(newReader);
+                  newReader.loadContent().subscribe(() => {
+                    // EPub is now rendered
+                  });
+                },
+              });
+            } catch (err) {
+              console.error(err);
+            }
+          })();
+        }}
+      />
+      <div id="epub-reader" className="mx-auto h-[800px] max-h-[80vh]"></div>
+
+      <div>
+        <button
+          onClick={() => {
+            console.log("prevPage");
+            reader?.previousPage();
+          }}
+        >
+          Prev page
+        </button>
+        <button
+          onClick={() => {
+            console.log("nextPage");
+            reader?.nextPage();
+          }}
+        >
+          Next page
+        </button>
+      </div>
     </>
   );
 }
--- retainify/src/components/FileInput.tsx
+++ retainify/src/components/FileInput.tsx
@@ -2,41 +2,11 @@ import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Book, createLoader, Reader } from "@asteasolutions/epub-reader";
 
-export function FileInput() {
+export function FileInput({ onChange }: { onChange }) {
   return (
     <div className="grid w-full max-w-sm items-center gap-3">
       <Label htmlFor="picture">Picture</Label>
-      <Input
-        id="picture"
-        type="file"
-        onChange={(e) => {
-          const file = e.target?.files?.[0];
-          console.log(file);
-          if (!file) {
-            return;
-          }
-
-          (async () => {
-            try {
-              const container = document.getElementById("epub-reader");
-              if (!container) return <></>;
-
-              const loader = await createLoader(file);
-              Book.open(loader).subscribe({
-                next: (book) => {
-                  const reader = new Reader(container, book);
-
-                  reader.loadContent().subscribe(() => {
-                    // EPub is now rendered
-                  });
-                },
-              });
-            } catch (err) {
-              console.error(err);
-            }
-          })();
-        }}
-      />
+      <Input id="picture" type="file" onChange={onChange} />
     </div>
   );
 }
